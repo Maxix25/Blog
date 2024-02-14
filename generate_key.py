8b208230e207130577321d72f02c556a39d65c0e
Maxi <maximiliano.besoain@gmail.com>
2024-02-14 12:48:23 -0300

    Open file in mode w+

--- generate_key.py
+++ generate_key.py
@@ -1,5 +1,5 @@
 from django.core.management.utils import get_random_secret_key  
 secret_key = get_random_secret_key()
 
-with open(".env", "w") as f:
+with open(".env", "w+") as f:
     f.write(f"SECRET_KEY={secret_key}")
