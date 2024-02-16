05f170975764460f9c70c623521ee08690835fd2
Maxi <maximiliano.besoain@gmail.com>
2024-02-16 00:31:57 -0300

    Add tests for staticpages

--- staticpages/tests.py
+++ staticpages/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class HomepageTest(TestCase):
+    # TODO: Finish tests
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username = 'testuser100', password = 'testpassword')
+        self.client.login(username = 'testuser100', password = 'testpassword')
+
+    def test_authenticated_navbar(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'Settings')
+    
+    def test_unauthenticated_navbar(self):
+        self.client.logout()
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'Login')
--- staticpages/urls.py
+++ staticpages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage),
-    path('404/', views.http_404)
+    path('', views.homepage, name = 'home'),
+    path('404/', views.http_404, name = '404')
 ]
